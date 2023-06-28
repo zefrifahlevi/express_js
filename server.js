9a817d7ee88935e474cbe183291ee19284e65e1a
Zefri <68502472+zefrifahlevi@users.noreply.github.com>
2023-06-28 18:49:01 +0700

    Update Data

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const req = require("express/lib/request");
 const res = require("express/lib/response");
 const app = express()
 
-/* Metode Pertama Middleware
+/* Metode Pertama Middleware 
 const zefri = (req, res, next) => {
     console.log("via zefri")
     next()
@@ -11,25 +11,27 @@ const zefri = (req, res, next) => {
 */
 
 /* Metode Panggil Variable Middleware
-app.get("/olivia",middleware, (req, res) => {
+app.get("/olivia",zefri, (req, res) => {
     res.send("Hai saya Olivia")
 })
 
-app.get("/drian",middleware, (req, res) => {
+app.get("/drian",zefri, (req, res) => {
     res.send("Ini Drian")
 })
 */
 
 const middleware = (req, res, next) => {
     if (req.query.via == "zefri") {
-            next()
+        next()
+        console.log("Anda masuk via zefri")
     }
     else {
         res.send("Dilarang!")
+        console.log("Tidak Bisa Masuk")
     }
 }
 
-app.use(middleware)
+app.use(middleware) 
 
 app.get("/olivia", (req, res) => {
     res.send("Hai saya Olivia")
@@ -37,6 +39,10 @@ app.get("/olivia", (req, res) => {
 
 app.get("/drian", (req, res) => {
     res.send("Ini Drian")
+})
+
+app.get("/zefa", (req, res) => {
+    res.send("ini azefa")
 })
 
 app.listen(80, () => console.log("server starting..."))
